b86088e036a8594f8caa585b016387710426957a
Rohan Rajput <rohanrajput32843@gmail.com>
2019-09-24 15:22:13 +0530

    upload file on s3 bucket

---
+++ server.js
@@ -0,0 +1,23 @@
+const fs = require('fs');
+const AWS = require('aws-sdk');
+const s3 = new AWS.S3({
+    accessKeyId: process.env.AWS_ACCESS_KEY,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
+});
+const fileName = 'contacts.csv';
+const uploadFile = () => {
+  fs.readFile(fileName, (err, data) => {
+     if (err) throw err;
+     const params = {
+         Bucket: 'testBucket', 
+         Key: 'contacts.csv', 
+         Body: JSON.stringify(data, null, 2)
+     };
+     s3.upload(params, function(s3Err, data) {
+         if (s3Err) throw s3Err
+         console.log(`File uploaded successfully at ${data.Location}`)
+     });
+  });
+};
+
+uploadFile();
